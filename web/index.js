a5ef9cca647cdb13fe02aba0281710012009deba
Ilya Bylich <ibylich@gmail.com>
2020-11-11 20:28:10 +0300

    deploy web/dist; parse initial input too

--- web/index.js
+++ web/index.js
@@ -5,6 +5,9 @@ console.log(wasm.js_parse("2 + 2"));
 let input = document.getElementById("input");
 let output = document.getElementById("output");
 
-input.onkeyup = () => {
+let parse = () => {
     output.value = wasm.js_parse(input.value);
 }
+
+input.onkeyup = parse;
+parse();
