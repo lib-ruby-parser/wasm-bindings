7b7fa8f1e1ea6576b191afc4a1c54c72f09b2be1
Ilya Bylich <ibylich@gmail.com>
2021-12-08 21:11:38 +0300

    fix ci

--- tests/build.mk
+++ tests/build.mk
@@ -1,5 +1,5 @@
 tests/no-modules: build/no-modules/lib_ruby_parser.js
 	node tests/test-web.js
 
-tests/node: build/nodejs/lib_ruby_parser.js
+tests/nodejs: build/nodejs/lib_ruby_parser.js
 	node tests/test-node.js
